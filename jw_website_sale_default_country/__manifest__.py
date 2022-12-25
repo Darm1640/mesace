761ad02dfe7bdf05ffe58a1e30e58dc09b724105
Darm1640 <93557532+Darm1640@users.noreply.github.com>
2022-12-25 09:50:22 -0500

    13

--- jw_website_sale_default_country/__manifest__.py
+++ jw_website_sale_default_country/__manifest__.py
@@ -19,7 +19,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Website',
+    'category': 'Website ',
     'version': '1.1',
 
     # any module necessary for this one to work correctly
